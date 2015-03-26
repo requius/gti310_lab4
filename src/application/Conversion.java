cea5f12c3e4eec4d79e39d54bb8e12c42e5a95b8
aj89610 <aj89610@10.196.122.192>
2015-03-26 17:06:16 -0400

    Conversion

---
+++ src/application/Conversion.java
@@ -0,0 +1,50 @@
+package application;
+
+public class Conversion {
+
+	public Conversion(){
+		
+	}
+	/*
+	 * PAS TESTER
+	 */
+	public int[][][] conversionRGBaYCbCr(int[][][] imageRGB){
+		int[][][] imageYCbCr = new int[3][][];
+		
+		for (int i = 0; i < imageRGB[0].length; i++) {
+			for (int j = 0; j < imageRGB[0][0].length; j++) {
+				int R = imageRGB[0][i][j];
+				int G = imageRGB[1][i][j];
+				int B = imageRGB[2][i][j];
+				
+				imageYCbCr[0][i][j] = (int)((0.299 * R) 		+ (0.587 * G)		+ (0.114 * B));
+				imageYCbCr[1][i][j] = (int)((-0.168736 * R) 	+ (-0.331264 * G)	+ (0.5 * B)) + 128;
+				imageYCbCr[2][i][j] = (int)((0.5 * R) 			+ (-0.418688 * G) 	+ (-0.081312 * B)) + 128;
+			}  
+		}
+		
+		return imageYCbCr;
+	}
+	
+	/*
+	 * PAS TESTER
+	 */
+	public int[][][] conversionYCbCraRGB(int[][][] imageYCbCr){
+		int[][][] imageRGB = new int[3][][];
+		
+		for (int i = 0; i < imageYCbCr[0].length; i++) {
+			for (int j = 0; j < imageYCbCr[0][0].length; j++) {
+				int Y = imageYCbCr[0][i][j];
+				int Cb = imageYCbCr[1][i][j];
+				int Cr = imageYCbCr[2][i][j];
+				
+				imageRGB[0][i][j] = (int)((1 * Y) 		+ (1.402 * Cr));
+				imageRGB[1][i][j] = (int)((1 * Y) 	+ (-0.344136 * Cb)	+ (-0.714136 * Cr)) - 128;
+				imageRGB[2][i][j] = (int)((1 * Y) 	+ (1.772 * Cb)) 	 - 128;
+			}  
+		}
+		
+		return imageRGB;
+	}
+	
+}
